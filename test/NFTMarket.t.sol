9200b3dbbaddf4e1b4aeb5254976535bb662bef4
Soso <husosong@163.com>
2025-01-05 21:18:59 +0800

    Setup for testing

--- test/NFTMarket.t.sol
+++ test/NFTMarket.t.sol
@@ -3,11 +3,20 @@ pragma solidity ^0.8.13;
 
 import { Test, console } from 'forge-std/Test.sol';
 import { NFTMarket } from '../src/NFTs/NFTMarket.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
 contract NFTMarketTest is Test {
     NFTMarket public nftMarket;
+    ERC20 public token;
+    ERC721 public nft;
 
     function setUp() public {
-        nftMarket = new NFTMarket();
+        token = new ERC20('Token', 'TOK', 18);
+        nft = new ERC721('NFT', 'NFT');
+
+        NFTMarket = new NFTMarket(address(nft), address(token));
+    }
+
     }
 }
