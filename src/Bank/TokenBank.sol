1f8c02c423c88561a9bbbd62e5c1bf4bc8dc572b
Soso <husosong@163.com>
2025-01-07 20:22:59 +0800

    Complete deposit function with amount

--- src/Bank/TokenBank.sol
+++ src/Bank/TokenBank.sol
@@ -4,7 +4,26 @@ pragma solidity ^0.8.28;
 import { SoToken } from '../NFTs/MyToken.sol';
 
 contract TokenBank {
-    constructor() {}
-    function deposit() public {}
+    SoToken token;
+    mapping(address => uint256) private balances;
+
+    error AmountGreaterThanZero();
+
+    event Deposit(address indexed user, uint256 amount);
+
+    constructor(address _token) {
+        token = SoToken(_token);
+    }
+    function deposit(uint amount) public {
+        if (amount == 0) {
+            revert AmountGreaterThanZero();
+        }
+
+        token.transfer(address(this), amount);
+        balances[msg.sender] += amount;
+
+        emit Deposit(msg.sender, amount);
+    }
+
     function withdraw() public {}
 }
