16c684dd951856fb0e34040b3da53604797af32d
Soso <husosong@163.com>
2025-01-07 20:27:07 +0800

    Init Bank with deposit, withdraw and top3depositor functions

---
+++ src/Bank/Bank.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Bank {
+    constructor() {}
+    function deposit() public {}
+    function withdraw() public {}
+    function top3Depositor() public view returns (address) {}
+}
