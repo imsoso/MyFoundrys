e9980dded5a9df56f8687566b6e5ca5f795f4b05
Soso <husosong@163.com>
2025-01-07 15:27:34 +0800

    Init TokenBank with deposit and withdraw functions

---
+++ src/Bank/TokenBank.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import { SoToken } from '../NFTs/MyToken.sol';
+
+contract TokenBank {
+    constructor() {}
+    function deposit() public {}
+    function withdraw() public {}
+}
