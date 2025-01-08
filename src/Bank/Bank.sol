bd04b182fbc3e820b8f8e5b6337892ba0431936e
Soso <husosong@163.com>
2025-01-08 21:02:41 +0800

    Use receive callback to accept eth transfer

--- src/Bank/Bank.sol
+++ src/Bank/Bank.sol
@@ -2,8 +2,27 @@
 pragma solidity ^0.8.28;
 
 contract Bank {
+    mapping(address => uint256) public balances;
+
+    error DepositMustGreaterTHanZero();
+
+    event Deposit(address indexed user, uint256 amount);
+
     constructor() {}
-    function deposit() public {}
+    function deposit() public payable {
+        if (msg.value == 0) {
+            revert DepositMustGreaterTHanZero();
+        }
+
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    // Receive ETH
+    receive() external payable {
+        deposit();
+    }
+
     function withdraw() public {}
     function top3Depositor() public view returns (address) {}
 }
