49686c03ce4d31a534a00582eb59d57ed70fd754
Soso <husosong@163.com>
2025-01-10 22:12:54 +0800

    Add owner for admin

--- src/Bank/BankAdmin.sol
+++ src/Bank/BankAdmin.sol
@@ -3,5 +3,9 @@ pragma solidity ^0.8.28;
 import { IBank } from './IBank.sol';
 contract Admin {
     address owner;
+    constructor() {
+        owner = msg.sender;
+    }
+
     function adminWithdraw(IBank bank) public {}
 }
