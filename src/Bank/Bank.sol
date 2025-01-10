96352a23246ba9f0950972783b536ba1b1806fea
Soso <husosong@163.com>
2025-01-10 17:13:40 +0800

    Only owner can withdraw

--- src/Bank/Bank.sol
+++ src/Bank/Bank.sol
@@ -5,10 +5,15 @@ contract Bank {
     mapping(address => uint256) public balances;
 
     error DepositMustGreaterThanZero();
+    error InfufficientBalance();
 
     event Deposit(address indexed user, uint256 amount);
+    event Withdraw(uint256 amount);
 
-    constructor() {}
+    constructor() {
+        owner = msg.sender;
+    }
+
     function deposit() public payable {
         if (msg.value == 0) {
             revert DepositMustGreaterThanZero();
@@ -23,6 +28,20 @@ contract Bank {
         deposit();
     }
 
-    function withdraw() public {}
-    function top3Depositor() public view returns (address) {}
+    modifier onlyOwner() {
+        require(msg.sender == address(this), 'Owner only');
+        _;
+    }
+
+    function withdraw(uint amount) public onlyOwner {
+        if (balances[msg.sender] < amount) {
+            revert InfufficientBalance();
+        }
+
+        (bool success, ) = address(this).call{ value: amount }('');
+        if (success) {
+            balances[msg.sender] -= amount;
+            emit Withdraw(amount);
+        }
+    }
 }
