c7c826d4c96ef4d78ddc7d4e906d6c67598ae138
Soso <husosong@163.com>
2025-01-10 20:09:49 +0800

    import IBank

--- src/Bank/BankAdmin.sol
+++ src/Bank/BankAdmin.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
-
+import { IBank } from './IBank.sol';
 contract Admin {
     address owner;
     function adminWithdraw(IBank bank) public {}
