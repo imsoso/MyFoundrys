c77b428a39fbb4abb74ec8bf1537b03d67bd8a70
Soso <husosong@163.com>
2025-01-10 21:54:59 +0800

    Add support for transfer admin

--- src/Bank/BigBank.sol
+++ src/Bank/BigBank.sol
@@ -3,12 +3,32 @@ pragma solidity ^0.8.28;
 import './Bank.sol';
 
 contract BigBank is Bank {
+    address public admin;
+
     error DepositTooSmall();
+    error NotAdmin();
+    error IllegalAddress;
+    constructor() {
+        admin = msg.sender;
+    }
 
     modifier greaterThanOneFinney() {
         if (msg.value <= 1 finney) {
             revert DepositTooSmall();
         }
         _;
+    }
+
+    function transferAdmin(address newAdmin) external {
+        if (msg.sender != admin) {
+            revert NotAdmin();
+        }
+
+        if(newAdmin == address(0)) {
+            revert IllegalAddress(;)
+        }
+    
+        admin = newAdmin;
+        
     }
 }
