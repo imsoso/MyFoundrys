13e6d342026a6c2a8f046dd20620182cc04890a7
Soso <husosong@163.com>
2025-01-10 21:49:46 +0800

    Add modifier to control greater than 1 finney

--- src/Bank/BigBank.sol
+++ src/Bank/BigBank.sol
@@ -2,4 +2,13 @@
 pragma solidity ^0.8.28;
 import './Bank.sol';
 
-contract BigBank is Bank {}
+contract BigBank is Bank {
+    error DepositTooSmall();
+
+    modifier greaterThanOneFinney() {
+        if (msg.value <= 1 finney) {
+            revert DepositTooSmall();
+        }
+        _;
+    }
+}
