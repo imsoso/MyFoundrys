28a46f960b4cc70519bbb4d2626e09e34305825e
Soso <husosong@163.com>
2025-01-16 20:10:54 +0800

    Feat: Create subclass of ERC20
    add function transferWithCallback to call target tokensReceived

---
+++ src/BaseTokens/TokenWithCallback.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+
+contract TokenWithCallback is ERC20, Ownable {
+    constructor(address initialOwner) ERC20('MyTokenCall', 'MTKC') Ownable(initialOwner) {}
+
+    function transferWithCallback(address to, uint256 amount) public returns (bool) {
+        bool success = transfer(to, amount);
+        if (isContract(to)) {
+            // Call target contract tokensReceived()
+            (success, ) = to.call(abi.encodeWithSignature('tokensReceived(address,uint256)', msg.sender, amount));
+            require(success, 'tokensReceived fail');
+        }
+
+        return true;
+    }
+
+    function isContract(address addr) public view returns (bool) {
+        return addr.code.length != 0;
+    }
+}
