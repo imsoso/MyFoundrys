ae19d4161ed8d7d10bad8f6ed1ce6acaa0e197be
Soso <husosong@163.com>
2025-01-17 19:52:05 +0800

    feat: init test with token、NFT and NFTMaket

--- test/NFTMarket.t.sol
+++ test/NFTMarket.t.sol
@@ -3,19 +3,22 @@ pragma solidity ^0.8.13;
 
 import { Test, console } from 'forge-std/Test.sol';
 import { NFTMarket } from '../src/NFTs/NFTMarket.sol';
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '../src/BaseTokens/TokenWithCallback.sol';
+import '../src/NFTs/MyNFT.sol';
 
 contract NFTMarketTest is Test {
     NFTMarket public nftMarket;
-    ERC20 public token;
-    ERC721 public nft;
+    TokenWithCallback public token;
+    SoNFT public nft;
+
+    address public owner;
 
     function setUp() public {
-        token = new ERC20('Token', 'TOK', 18);
-        nft = new ERC721('NFT', 'NFT');
+        owner = address(this);
+        token = new TokenWithCallback(owner);
+        nft = new SoNFT(owner);
 
-        NFTMarket = new NFTMarket(address(nft), address(token));
+        nftMarket = new NFTMarket(address(nft), address(token));
     }
 
     }
