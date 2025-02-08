8858295ee34f450d625c1eceb2717962e055bc7e
Soso <husosong@163.com>
2025-02-08 21:55:58 +0800

    feat: init contract with all signers and threshold

--- src/Wallet/MultiSignWallet.sol
+++ src/Wallet/MultiSignWallet.sol
@@ -1,3 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
-contract MultiSignWallet {}
+
+contract MultiSignWallet {
+    // List of multi-signature owners
+    address[] public signers;
+    // Signature threshold
+    uint public threshold;
+
+    constructor(address[] memory _signers, uint _threshold) {
+        signers = _signers;
+        threshold = _threshold;
+    }
+}
