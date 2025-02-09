d2e222ed79947ba760d245416605cb6da659a589
Soso <husosong@163.com>
2025-02-09 10:50:33 +0800

    feat: initiate a proposal
    Create a proposal struct and add to proposal list

--- src/Wallet/MultiSignWallet.sol
+++ src/Wallet/MultiSignWallet.sol
@@ -7,8 +7,27 @@ contract MultiSignWallet {
     // Signature threshold
     uint public threshold;
 
+    uint256 public proposalNumber;
+
+    struct Proposal {
+        address to;
+        uint value;
+        bytes data;
+        uint approvals;
+    }
+    mapping(uint256 => Proposal) public proposals;
+
+    event ProposalInitiate(uint256 indexed proposalID, address to, uint256 value, bytes data, uint256 approvals);
     constructor(address[] memory _signers, uint _threshold) {
         signers = _signers;
         threshold = _threshold;
+    }
+
+    function initiateProposal(address _to, uint _value, bytes memory _data) public {
+        // Create a new proposal
+        uint number = proposalNumber++;
+        proposals[number] = Proposal({ to: _to, value: _value, data: _data, approvals: 0 });
+
+        emit ProposalInitiate(number, _to, _value, _data, 0);
     }
 }
