029515aa02f0913f0883aa7cd1f5c4060741ba50
Soso <husosong@163.com>
2025-02-09 11:25:33 +0800

    refact: initiateProposal function

--- src/Wallet/MultiSignWallet.sol
+++ src/Wallet/MultiSignWallet.sol
@@ -19,7 +19,7 @@ contract MultiSignWallet {
 
     error IlegalSigner();
 
-    event ProposalInitiate(uint256 indexed proposalID, address to, uint256 value, bytes data, uint256 approvals);
+    event ProposalInitiate(uint256 indexed proposalID, address to, uint256 value, bytes data);
     constructor(address[] memory _signers, uint _threshold) {
         signers = _signers;
         threshold = _threshold;
@@ -42,10 +42,16 @@ contract MultiSignWallet {
         return false;
     }
 
-    function initiateProposal(address _to, uint _value, bytes memory _data) public onlySigner {
+    function initiateProposal(address to, uint value, bytes memory data) public onlySigner {
         // Create a new proposal
         uint number = proposalNumber++;
-        proposals[number] = Proposal({ to: _to, value: _value, data: _data, approvals: 0 });
+        Proposal storage proposal = proposals[number];
+        proposal.to = to;
+        proposal.value = value;
+        proposal.data = data;
+
+        emit ProposalInitiate(number, to, value, data);
+    }
 
         emit ProposalInitiate(number, _to, _value, _data, 0);
     }
