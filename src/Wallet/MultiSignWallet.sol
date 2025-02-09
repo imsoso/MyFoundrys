abd4195d9f8a61125bd9ed241460693d72a2853d
Soso <husosong@163.com>
2025-02-09 11:14:38 +0800

    feat:  limit for only signer
    Only signer can initiateProposal

--- src/Wallet/MultiSignWallet.sol
+++ src/Wallet/MultiSignWallet.sol
@@ -17,13 +17,32 @@ contract MultiSignWallet {
     }
     mapping(uint256 => Proposal) public proposals;
 
+    error IlegalSigner();
+
     event ProposalInitiate(uint256 indexed proposalID, address to, uint256 value, bytes data, uint256 approvals);
     constructor(address[] memory _signers, uint _threshold) {
         signers = _signers;
         threshold = _threshold;
     }
 
-    function initiateProposal(address _to, uint _value, bytes memory _data) public {
+    modifier onlySigner() {
+        if (!isSigner(msg.sender)) {
+            revert IlegalSigner();
+        }
+        _;
+    }
+
+    // Check if an address is an owner
+    function isSigner(address addr) public view returns (bool) {
+        for (uint256 i = 0; i < signers.length; i++) {
+            if (signers[i] == addr) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function initiateProposal(address _to, uint _value, bytes memory _data) public onlySigner {
         // Create a new proposal
         uint number = proposalNumber++;
         proposals[number] = Proposal({ to: _to, value: _value, data: _data, approvals: 0 });
