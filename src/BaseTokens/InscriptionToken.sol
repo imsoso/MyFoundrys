9ee69148dabd23829e009b8ec582256e3abd5e47
Soso <husosong@163.com>
2025-03-21 17:54:54 +0800

    feat: remove uupsupgradeable add name and symbol as parameters

--- src/BaseTokens/InscriptionToken.sol
+++ src/BaseTokens/InscriptionToken.sol
@@ -1,23 +1,23 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.28;
 
-import { ERC20Upgradeable } from '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
-import { Initializable } from '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
-import { OwnableUpgradeable } from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
-import { UUPSUpgradeable } from '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
+import { ERC20Upgradeable } from '@openzeppelin-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol';
+import { Initializable } from '@openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol';
+import { OwnableUpgradeable } from '@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol';
 
-contract MyToken is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+contract InscriptionToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(address initialOwner) public initializer {
-        __ERC20_init('MyInscription', 'MIT');
+    function initialize(string memory name, string memory symbol, address initialOwner) external initializer {
+        __ERC20_init(name, symbol);
         __Ownable_init(initialOwner);
-        __UUPSUpgradeable_init();
     }
 
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
 }
