35718303ecf7a631df1e1a97c1f538ef81fd5a45
Soso <husosong@163.com>
2025-03-21 21:09:12 +0800

    feat: deployInscription using clone

--- src/NFTs/MyInscription.sol
+++ src/NFTs/MyInscription.sol
@@ -9,13 +9,20 @@ import { UUPSUpgradeable } from '@openzeppelin-upgradeable/contracts/proxy/utils
 import { InscriptionToken } from '../BaseTokens/InscriptionToken.sol';
 
 contract MyInscription is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    InscriptionToken aToken;
     // token struct
     struct TokenInfo {
         uint256 totalSupply; // total supply
         uint256 perMint; // per mint amount
         uint256 mintedAmount; // minted amount
     }
+    mapping(address => TokenInfo) public tokenInfo;
+
     address public implementationContract;
+
+    error PerMintExceedsTotalSupply();
+
+    event InscriptionDeployed(address indexed tokenAddress, string symbol, uint256 totalSupply, uint256 perMint);
 
     constructor() {
         // disable initializer
@@ -29,6 +36,19 @@ contract MyInscription is Initializable, UUPSUpgradeable, OwnableUpgradeable {
         // Deploy a token implementation contract as template
         InscriptionToken tokenImpl = new InscriptionToken();
         implementationContract = address(tokenImpl);
+    }
+
+    function deployInscription(string memory symbol, uint totalSupply, uint perMint) external returns (address) {
+        // check per mint amount
+        if (perMint > totalSupply) revert PerMintExceedsTotalSupply();
+
+        address newToken = Clones.clone(implementationContract);
+        InscriptionToken(newToken).initialize('MyInscriptionToken', 'MIT', address(this));
+
+        tokenInfo[newToken] = TokenInfo({ totalSupply: totalSupply, perMint: perMint, mintedAmount: 0 });
+
+        emit InscriptionDeployed(newToken, symbol, totalSupply, perMint);
+        return newToken;
     }
 
     // authorize upgrade(only owner)
