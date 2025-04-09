847406d2aedf6fa3bf7082608e0f0241b4245292
Soso <husosong@163.com>
2025-04-09 15:11:10 +0800

    feat: esRNT token with Burnable

---
+++ src/BaseTokens/BurnableToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.28;
+
+import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import { ERC20Burnable } from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+
+contract esRNT is ERC20, ERC20Burnable, Ownable {
+    constructor(address recipient, address initialOwner) ERC20('esRNT', 'esRNT') Ownable(initialOwner) {
+        _mint(recipient, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
