63cfc4f83b4314cf6319e531efd3663b1f4745ea
Soso <husosong@163.com>
2025-04-11 18:27:37 +0800

    feat: Add a new lock entry for the recipient with the specified amount and the current timestamp as the lock time
    Everytime User Mint -> Lock esRNT to claim RNT

--- src/BaseTokens/BurnableToken.sol
+++ src/BaseTokens/BurnableToken.sol
@@ -9,6 +9,15 @@ import './ERC20WithPermit.sol';
 
 contract esRNT is ERC20, ERC20Burnable, Ownable {
     SoToken public RNTToken;
+
+    struct LockInfo {
+        uint256 amount;
+        uint256 lockTime;
+    }
+    mapping(address => LockInfo[]) public lockInfos;
+
+    error InsufficientBalance();
+
     constructor(address recipient, address initialOwner, address _RNTToken) ERC20('esRNT', 'esRNT') Ownable(initialOwner) {
         RNTToken = SoToken(_RNTToken);
         _mint(recipient, 1000000 * 10 ** decimals());
@@ -16,5 +25,13 @@ contract esRNT is ERC20, ERC20Burnable, Ownable {
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+        // Add a new lock entry for the recipient with the specified amount and the current timestamp as the lock time
+        lockInfos[to].push(
+            LockInfo({
+                amount: amount, // The amount to lock
+                lockTime: block.timestamp // The time at which the lock is created
+            })
+        );
+    }
     }
 }
