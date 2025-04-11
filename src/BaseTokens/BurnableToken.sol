801c53ca85520abe4b4a0c1206603c347d3ac607
Soso <husosong@163.com>
2025-04-11 18:25:45 +0800

    feat: pass RNT Token address to use contract

--- src/BaseTokens/BurnableToken.sol
+++ src/BaseTokens/BurnableToken.sol
@@ -5,9 +5,12 @@ pragma solidity ^0.8.28;
 import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import { ERC20Burnable } from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
 import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+import './ERC20WithPermit.sol';
 
 contract esRNT is ERC20, ERC20Burnable, Ownable {
-    constructor(address recipient, address initialOwner) ERC20('esRNT', 'esRNT') Ownable(initialOwner) {
+    SoToken public RNTToken;
+    constructor(address recipient, address initialOwner, address _RNTToken) ERC20('esRNT', 'esRNT') Ownable(initialOwner) {
+        RNTToken = SoToken(_RNTToken);
         _mint(recipient, 1000000 * 10 ** decimals());
     }
 
