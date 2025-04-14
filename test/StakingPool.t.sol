68635a8b1769145c97cab1f19bc62a0adb6d843e
Soso <husosong@163.com>
2025-04-14 08:53:43 +0800

    feat: init StakingPoolTest

---
+++ test/StakingPool.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import 'forge-std/Test.sol';
+import 'forge-std/StdUtils.sol';
+import '../src/BaseTokens/ERC20WithPermit.sol';
+import '../src/BaseTokens/BurnableToken.sol';
+
+contract StakingPoolTest is Test {
+    esRNT public esRNTToken;
+    SoToken public RNTToken;
+    function setUp() public {}
+}
