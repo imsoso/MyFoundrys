2cc716d603ec5dd0b0aca820f0a1d91a7c83797c
Soso <husosong@163.com>
2025-04-18 19:12:43 +0800

    feat: use ERC20Upgradeable and OwnableUpgradeable to initialize

--- src/Upgrade/InscriptionToken.sol
+++ src/Upgrade/InscriptionToken.sol
@@ -2,20 +2,20 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.28;
 
-// import { ERC20Upgradeable } from '@openzeppelin-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol';
+import { ERC20Upgradeable } from '@openzeppelin-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol';
+import { OwnableUpgradeable } from '@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol';
 import { Initializable } from '@openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol';
-import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
-// import { OwnableUpgradeable } from '@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol';
 
-contract InscriptionToken is Initializable, ERC20, Ownable {
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
+contract InscriptionToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+    // 构造函数：仅用于 new 部署（内部调用 initialize）
+    constructor(string memory name, string memory symbol, address initialOwner) {
+        // 直接调用 initialize（模拟升级合约的初始化）
+        initialize(name, symbol, initialOwner);
     }
 
-    function initialize(string memory name, string memory symbol, address initialOwner) external initializer {
-        require(bytes(name()).length == 0, 'Already initialized');
+    function initialize(string memory name, string memory symbol, address initialOwner) public initializer {
+        __ERC20_init(name, symbol);
+        __Ownable_init(initialOwner);
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
