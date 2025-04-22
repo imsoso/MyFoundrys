4742ed0d8b71edc2d7b3fb51d3a37ead2b3c5565
Soso <husosong@163.com>
2025-04-22 17:42:40 +0800

    test: V1_DeployAndMint

--- test/InscriptionFactoryUpgrade.t.sol
+++ test/InscriptionFactoryUpgrade.t.sol
@@ -9,9 +9,9 @@ import { InscriptionToken } from '../src/Upgrade/InscriptionToken.sol';
 import { Upgrades } from 'openzeppelin-foundry-upgrades/Upgrades.sol';
 
 contract InscriptionFactoryUpgradeTest is Test {
-    InscriptionToken public implementationToken;
-    InscriptionFactoryV1 public factoryV1Implementation;
-    InscriptionFactoryV2 public factoryV2Implementation;
+    InscriptionToken public inscriptionToken;
+    InscriptionFactoryV1 public factoryV1;
+    InscriptionFactoryV2 public factoryV2;
 
     address public owner;
     address public user1;
@@ -21,11 +21,31 @@ contract InscriptionFactoryUpgradeTest is Test {
         owner = address(this);
         user1 = makeAddr('user1');
         user2 = makeAddr('user2');
-
-        address proxy = Upgrades.deployUUPSProxy('MyUUPSProxy.sol', abi.encodeCall(InscriptionFactoryV1.initialize, owner));
-
+        address proxy = Upgrades.deployUUPSProxy('InscriptionFactoryV1.sol', abi.encodeCall(InscriptionFactoryV1.initialize, owner));
+        factoryV1 = InscriptionFactoryV1(proxy);
         // Fund test users
         vm.deal(user1, 100 ether);
         vm.deal(user2, 100 ether);
+    }
+
+    // --- Test V1 Functionality Before Upgrade ---
+
+    function test_V1_DeployAndMint() public {
+        // Deploy a token using V1
+        address tokenAddrV1 = factoryV1.deployInscription('TOKENV1', 1000, 10);
+        assertTrue(tokenAddrV1 != address(0), 'V1 deployment failed');
+        (uint256 ts, uint256 pm, uint256 ma) = factoryV1.tokenInfos(tokenAddrV1);
+        assertEq(ts, 1000, 'V1 total supply mismatch');
+        assertEq(pm, 10, 'V1 per mint mismatch');
+        assertEq(ma, 0, 'V1 initial minted amount mismatch');
+        // Price should be default 0 for V1 struct slot, even if accessed via V2 interface later
+
+        // Mint using V1
+        vm.prank(user1);
+        factoryV1.mintInscription(tokenAddrV1);
+        InscriptionToken token = InscriptionToken(tokenAddrV1);
+        assertEq(token.balanceOf(user1), 10, 'V1 mint failed, user balance mismatch');
+        (, , ma) = factoryV1.tokenInfos(tokenAddrV1); // Get only mintedAmount
+        assertEq(ma, 10, 'V1 minted amount after mint mismatch');
     }
 }
