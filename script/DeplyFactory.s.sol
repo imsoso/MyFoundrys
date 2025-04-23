b11f54edb2d8b77ecef359f9f22524c74efc2297
Soso <husosong@163.com>
2025-04-23 10:58:45 +0800

    chore: init deploy script

---
+++ script/DeplyFactory.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import 'forge-std/Script.sol';
+import '../src/upgrade/InscriptionToken.sol';
+import '../src/upgrade/InscriptionFactoryV1.sol';
+import '../src/upgrade/InscriptionFactoryV2.sol';
+
+contract DeployScript is Script {
+    function run() external {}
+}
