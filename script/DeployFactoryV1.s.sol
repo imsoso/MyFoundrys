4b8669c6f35d78bef4fa04463bb916ab5331e7e5
Soso <husosong@163.com>
2025-04-24 15:38:04 +0800

    refactor: move and rename file

---
+++ script/DeployFactoryV1.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import 'forge-std/Script.sol';
+import '../src/upgrade/InscriptionToken.sol';
+import { InscriptionFactoryV1 } from '../src/Upgrade/InscriptionFactoryV1.sol';
+import { InscriptionFactoryV2 } from '../src/Upgrade/InscriptionFactoryV2.sol';
+import { Upgrades } from 'openzeppelin-foundry-upgrades/Upgrades.sol';
+
+contract DeployFactoryV1Script is Script {
+    function setUp() public {}
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint('PRIVATE_KEY');
+        address owner = vm.envAddress('OWNER_ADDRESS');
+
+        // Pre-compute the addresses for console output
+        address deployer = vm.addr(deployerPrivateKey);
+        console.log('Deploying contracts with address:', deployer);
+        console.log('Setting owner to:', owner);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        address proxy = Upgrades.deployUUPSProxy('InscriptionFactoryV1.sol', abi.encodeCall(InscriptionFactoryV1.initialize, owner));
+        console.log('Proxy deployed at:', address(proxy));
+
+        InscriptionFactoryV1 factoryV1 = InscriptionFactoryV1(proxy);
+
+        address tokenAddrV1 = factoryV1.deployInscription('TOKENV1', 1000, 10);
+
+        vm.stopBroadcast();
+
+        console.log('\n=== Deployment Summary ===');
+        console.log('Network:', block.chainid);
+        console.log('Inscription Token V1 Implementation:', tokenAddrV1);
+
+        console.log('Factory V1 Implementation:', address(factoryV1));
+
+        console.log('Proxy:', address(proxy));
+        console.log('Owner:', owner);
+        console.log('Deployer:', deployer);
+    }
+}
