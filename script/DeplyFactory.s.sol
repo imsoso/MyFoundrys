e5ff6789be07c14e53ba9efa47a9f5b65340981d
Soso <husosong@163.com>
2025-04-24 10:48:10 +0800

    fix: import contract explicitly, which will cause conflict with InscriptionFactoryUpgrade.t.sol
    import { InscriptionFactoryV1 } from '../src/Upgrade/InscriptionFactoryV1.sol'; is an explicit import that only brings in the InscriptionFactoryV1 contract or library, avoiding unnecessary imports and potential naming conflicts.
    On the other hand, import '../src/upgrade/InscriptionFactoryV1.sol'; is an implicit import that includes all definitions from the file, which might lead to naming conflicts if multiple contracts or libraries are defined in the file.
    Explicit imports are recommended for clarity and safety.

--- script/DeplyFactory.s.sol
+++ script/DeplyFactory.s.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.28;
 
 import 'forge-std/Script.sol';
 import '../src/upgrade/InscriptionToken.sol';
-import '../src/upgrade/InscriptionFactoryV1.sol';
-import '../src/upgrade/InscriptionFactoryV2.sol';
+import { InscriptionFactoryV1 } from '../src/Upgrade/InscriptionFactoryV1.sol';
+import { InscriptionFactoryV2 } from '../src/Upgrade/InscriptionFactoryV2.sol';
 
 contract DeployScript is Script {
+    function setUp() public {}
+
     function run() external {}
 }
