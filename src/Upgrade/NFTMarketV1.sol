50228861d3cb614ed91e3ad69f3cf9e8694456f7
Soso <husosong@163.com>
2025-04-26 17:09:16 +0800

    feat: add Initializable.sol for upgrading rename nftMarket to nftContract to avoid misunderstanding

--- src/Upgrade/NFTMarketV1.sol
+++ src/Upgrade/NFTMarketV1.sol
@@ -6,8 +6,11 @@ import '@openzeppelin/contracts/utils/Address.sol';
 import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
-contract NFTMarket is IERC721Receiver {
-    IERC721 public nftMarket;
+import '@openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol';
+import '@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol';
+
+contract NFTMarket is IERC721Receiver, Initializable, OwnableUpgradeable {
+    IERC721 public nftContract;
 
     IERC20 public nftToken;
 
@@ -18,17 +21,25 @@ contract NFTMarket is IERC721Receiver {
 
     mapping(uint256 => NFTProduct) public NFTList;
 
-    constructor(address _nftMarket, address _nftToken) {
-        nftMarket = IERC721(_nftMarket);
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    // initialize function
+    function initialize(address _nftContract, address _nftToken, address initialOwner) public initializer {
+        __Ownable_init(initialOwner);
+
+        nftContract = IERC721(_nftContract);
         nftToken = IERC20(_nftToken);
     }
 
     // List NFT on the market
     function list(uint256 tokenId, uint256 price) external {
-        require(nftMarket.ownerOf(tokenId) == msg.sender, 'You are not the owner');
+        require(nftContract.ownerOf(tokenId) == msg.sender, 'You are not the owner');
         require(price > 0, 'Price must be greater than zero');
         // Transfer NFT to the market, make it available for sale
-        nftMarket.safeTransferFrom(msg.sender, address(this), tokenId);
+        nftContract.safeTransferFrom(msg.sender, address(this), tokenId);
         NFTList[tokenId] = NFTProduct({ price: price, seller: msg.sender });
     }
     function buyNFT(address buyer, uint256 amount, uint256 nftId) public {
@@ -41,7 +52,7 @@ contract NFTMarket is IERC721Receiver {
         require(amount == aNFT.price, 'Insufficient token amount to buy NFT');
         require(nftToken.transferFrom(buyer, aNFT.seller, aNFT.price), 'Token transfer failed');
 
-        nftMarket.transferFrom(address(this), buyer, nftId);
+        nftContract.transferFrom(address(this), buyer, nftId);
         delete NFTList[nftId];
     }
 
@@ -52,7 +63,7 @@ contract NFTMarket is IERC721Receiver {
         require(aNFT.price > 0, 'NFT is not listed for sale');
         require(amount == aNFT.price, 'Incorrect payment amount');
 
-        nftMarket.safeTransferFrom(address(this), from, tokenId);
+        nftContract.safeTransferFrom(address(this), from, tokenId);
         nftToken.transfer(aNFT.seller, amount);
         delete NFTList[tokenId];
     }
